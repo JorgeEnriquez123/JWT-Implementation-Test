e772f3cd03d2e20fa6089398fcf40ea69bd3691f
JorgeEnriquez123 <joseph_adagio_7700@hotmail.com>
2024-01-09 21:25:16 -0500

    UserInfo Controller added. Postman Collection Added

--- src/main/java/com/jorge/jwtnewtest/controller/SecuredController.java
+++ src/main/java/com/jorge/jwtnewtest/controller/SecuredController.java
@@ -1,9 +1,15 @@
 package com.jorge.jwtnewtest.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/secured")
@@ -11,6 +17,14 @@ public class SecuredController {
     @GetMapping
     public String securedMessage1(){
         return "Secured Message 1";
+    }
+
+    @GetMapping("/userinfo")
+    public Map<String, String> userInfo(Authentication auth){
+        Map<String, String> userInfo = new HashMap<>();
+        userInfo.put("username", auth.getName());
+        userInfo.put("roles", auth.getAuthorities().toString());
+        return userInfo;
     }
 
     @PreAuthorize("hasAuthority('ADMIN')")
